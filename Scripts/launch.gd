5e8d2cff1f871de5a495565af2996dcc8f264eb7
= <ian@iansolutions.xyz>
2025-10-04 16:41:27 -0700

    Added basic card system

--- Scripts/launch.gd
+++ Scripts/launch.gd
@@ -4,34 +4,63 @@ extends Node
 @onready var learn_card: Control = $LearnCard
 const HIT_MARKER = preload("res://Scenes/hit_marker.tscn")
 
-var asteroids = []
 var current_index = 0
 
-func _ready():
-	var error = http_request.request("https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=mIYsnrgYn3Nh3zKR0V3IvSvQo4ghKAfpGDEEdBmE")
-	if error != OK:
-		print("An error occurred in the HTTP request.")
+var current_card = 0
 
-func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	#print(json)
-	# TODO error handing
-	for object in json["near_earth_objects"]:
-		#print(object["name"])
-		asteroids.append(object)
+const _1 = preload("uid://cvpgqgx1r3n34")
+const _2 = preload("uid://0k0qqn6x4l2u")
+const _3 = preload("uid://d1kohb7frrb6i")
+const _4 = preload("uid://def2spo520pc0")
+const _5 = preload("uid://b7rybkt6fsbgt")
+
+var asteroids = [_1, _2, _3, _4, _5]
+
+func _ready():
 	
-	learn_card.title.text = asteroids[current_index]["name"]
-	learn_card.info.text = str(asteroids[current_index].keys())
 	var pos = get_random_position()
 	var hit_marker = HIT_MARKER.instantiate()
 	hit_marker.position = pos
 	add_child(hit_marker)
-	
 
-var screen_width = 1920
-var screen_height = 1080
+
+var screen_width = 1920 / 0.9
+var screen_height = (1080 - 300) / 0.9
 
 func get_random_position() -> Vector2:
 	var x = randi_range(0, screen_width - 1)
 	var y = randi_range(0, screen_height - 1)
 	return Vector2(x, y)
+
+
+func _on_button_1_button_down() -> void:
+	if learn_card.visible == true:
+		if current_card == 1:
+			learn_card.hide()
+		else:
+			current_card = 1
+	else:
+		learn_card.show()
+		current_card = 1
+
+
+func _on_button_2_button_down() -> void:
+	if learn_card.visible == true:
+		if current_card == 2:
+			learn_card.hide()
+		else:
+			current_card = 2
+	else:
+		learn_card.show()
+		current_card = 2
+
+
+func _on_button_3_button_down() -> void:
+	if learn_card.visible == true:
+		if current_card == 3:
+			learn_card.hide()
+		else:
+			current_card = 3
+	else:
+		learn_card.show()
+		current_card = 3
