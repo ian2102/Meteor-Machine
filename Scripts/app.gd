0ff49ce7e433aa605353086d6eb19aea8cbc5e30
= <ian@iansolutions.xyz>
2025-10-04 14:06:43 -0700

    Added title and learn screans

--- Scripts/app.gd
+++ Scripts/app.gd
@@ -1,28 +1,35 @@
 extends Node
 
 @onready var http_request: HTTPRequest = $HTTPRequest
-@onready var label: Label = $CanvasLayer/Control/Label
+
+#const LAUNCH = preload("res://Scenes/launch.tscn")
+const LEARN = preload("res://Scenes/learn.tscn")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action("quit"):
 		get_tree().quit()
 
 func _ready():
-	var error = http_request.request("https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY")
-	if error != OK:
-		label.text = "An error occurred in the HTTP request."
+	pass
+	#var error = http_request.request("https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY")
+	#if error != OK:
+		#label.text = "An error occurred in the HTTP request."
 
 func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	label.text = ""
-	for object in json["near_earth_objects"]:
-		print(object["name"])
-		label.text += object["name"] + "\n"
+	pass
+	#var json = JSON.parse_string(body.get_string_from_utf8())
+	#label.text = ""
+	#print(json)
+	# TODO error handing
+	#for object in json["near_earth_objects"]:
+		#print(object["name"])
+		#label.text += object["name"] + "\n"
 
 
 func _on_learn_button_down() -> void:
-	pass # Replace with function body.
-
+	var learn = LEARN.instantiate()
+	add_child(learn)
 
 func _on_launch_button_down() -> void:
-	pass # Replace with function body.
+	var launch = LEARN.instantiate()
+	add_child(launch)
--- Scripts/learn.gd
+++ Scripts/learn.gd
@@ -1,1 +1,21 @@
 extends Node
+
+
+func _on_asteroids_button_down() -> void:
+	print("hello")
+
+
+func _on_mitigation_button_down() -> void:
+	print("hello")
+
+
+func _on_environmental_button_down() -> void:
+	print("hello")
+
+
+func _on_button_left_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_right_button_down() -> void:
+	pass # Replace with function body.
